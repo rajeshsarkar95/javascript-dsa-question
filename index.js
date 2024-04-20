468c48b319b6c6f007d0cfdd85d280f38147837a
rajesh <prorajeshsarkar@gmail.com>
2024-04-21 00:18:41 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,163 @@
+// var score = 65;
+// var  grade;
+// if(score >= 90){
+//   grade = 'A'
+// }else if (score >= 80){
+//   grade = "B"
+// }else if (score >= 70){
+//   grade = "C"
+// }else if(score >=60){
+//   grade = "D"
+// }else {
+//   grade = "F"
+// }
+// console.log('garde:' + grade)
+// var age = 25;
+// var tiketsPrice;
+// if (age < 12){
+//   tiketsPrice = 5;
+// }else if (age >= 12 &&  age <18){
+//   tiketsPrice = 10;
+// }else if (age >= 18 && age < 60){
+//   tiketsPrice = 20;
+// }else {
+//   tiketsPrice = 15
+// }
+// console.log("Tiket  price: $"+ tiketsPrice)
+
+
+
+// var yare = 2024;
+// if (yare % 4 === 0 && (yare % 100 !== 0 || yare %  400 === 0)){
+//   console.log(yare + "is a leap yere");
+// }else{
+//   console.log(yare + "is not a leap  yere")
+// }
+// shoping discount 
+// var purchaseAmount = 120;
+// var discount;
+
+// if (purchaseAmount >= 100) {
+//   discount = 20;
+// } else if (purchaseAmount >= 50) {
+//   discount = 10;
+// } else {
+//   discount = 0;
+// }
+// console.log("Discount: " + discount + "%");
+
+// var purchaseAmount = ;
+// var discount;
+// if(purchaseAmount >= 100){
+//   discount = 20;
+// }else if (purchaseAmount >= 50){
+//   disconut = 10;
+// }
+// console.log("discount " +  discount + " %")
+
+// var currentTime = new Date();
+// var currentHour = currentTime.getHours();
+// var greeting;
+// if (currentHour < 12){
+//   greting = "Good mornig";
+// }else if (currentHour < 18){
+//   greting = "good afternoon ";
+// }else {
+//   greeting = "Good evening"
+// }
+// console.log(greeting)
+// var currentTime =  new Date();
+// var currentHour = currentTime.getHours();
+// var greting;
+// if(currentHour < 12){
+//   greting = " Good morning";
+// }else if(currentHour < 18){
+//    greting = "Good afternoon";
+// }else {
+//   greting = "Good evening"
+// }
+// console.log(greting)
+
+
+// var weight = 70;
+// var height = 1.75;
+// var bmi = weight/(height* height);
+// var category;
+// if (bmi < 18.5){
+//   category = "underweight"; 
+// }else if (bmi < 24.9){
+//   category = "normal weigth"; 
+// }else if (bmi < 29.9){
+//   category = "overWeight"; 
+// }else {
+//   category = "obese"; 
+// }
+// console.log("BMI:" + bmi.toFixed(2));
+// console.log("categy " + category)
+
+// var weight = 70;
+// var height = 1.75;
+// var bmi = weight/(height*height);
+// var category;
+// if(bmi < 18.5){
+//   category = "underWeight"
+// }else if (bmi  < 24.9){
+//   category = "normal weight"
+// }else if (bmi < 29.9){
+//   category = "overweight"
+// }else {
+//   category = "obese";
+// }
+// console.log("BMI" +  bmi.toFixed(2));
+// console.log("categy" + category)
+
+// var secretNumber = 7;
+// var guess = 5;
+// if(guess === secretNumber){
+//   console.log("congratulation you guessed the corect number");
+// }else if (guess < secretNumber){
+//   console.log(guess < secretNumber)
+// }else {
+//   console.log("ty a lower number")
+// }
+// var secretNumber = 7;
+// var guess = 5; // The player's guess, change this to see that different code lines are executed based on the conditions
+// if (guess === secretNumber) {
+//   console.log("Congratulations! You guessed the correct number.");
+// } else if (guess < secretNumber) {
+//   console.log(10);
+// } else {
+//   console.log(0);
+// }
+// for (let i =12; i  <= 15; i++  ){
+//   console.log("value of i of " + i)
+// }
+// for (var i = 0; i <= 15; i++){
+//   console.log("value of i is" + i);
+// }
+// for 
+// for (var i = 7 ; i <= 31; i++){
+//   if (i % 2 != 0)
+//   console.log(i)
+// }
+// for (var i = 0; i <= 15; i++){
+//   if(i % 2 != 0)
+//   console.log(i)
+// }
+// for (var i= 7 ; i<= 15; i++){
+//   if(i % 2 !=0)
+//   console.log(i)
+// }
+// for (var i = 7; i<=15; i++){
+//   console.log(i % 2 !=0)
+//   console.log(i)
+// }
+// for (var i = 0; i<=31; i++){
+//   if(i % 2 != 0)
+//   console.log(i)
+// }
+for (var i = 10; i >= -20; i--){
+  if(i % 2 == 0 ){
+  console.log(i)
+}
+}
---
+++ logical.js
@@ -0,0 +1,80 @@
+// var age = 25;
+// var tiketsPrice;
+// if(age < 12){
+//   tiketsPrice = 5;
+// }else if (age >= 12 && age < 18){
+//   tiketsPrice = 10;
+// }else if (age >=18 && age < 60){
+//   tiketsPrice = 20;
+// }else{
+//   tiketsPrice = 15;
+// }
+// console.log("tiketPrice $" + tiketsPrice)
+
+// write the progarm for lefe yares
+
+// var age = 16;
+// var tiketPrice;
+
+// if(age < 12){
+//   tiketPrice = 5;
+// }else if (age >= 12 && age < 18){
+//   tiketPrice = 10;
+// }else if (age >= 18 && age < 60){
+//       tiketPrice = 20;
+// }else {
+//   tiketPrice = 15;
+// }
+// console.log("Tiket Price " + tiketPrice)
+
+// var purchaseAmount = 120;
+// var discount;
+
+// if (purchaseAmount >= 100) {
+//   discount = 20;
+// } else if (purchaseAmount >= 50) {
+//   discount = 10;
+// } else {
+//   discount = 0;
+// }
+// console.log("Discount: " + discount + "%");
+// var weight = 70; // in kilograms
+// var height = 1.75; // in meters
+// var bmi = weight / (height * height);
+// var category;
+
+// if (bmi < 18.5) {
+//   category = "Underweight";
+// } else if (bmi < 24.9) {
+//   category = "Normal weight";
+// } else if (bmi < 29.9) {
+//   category = "Overweight";
+// } else {
+//   category = "Obese";
+// }
+// console.log("BMI: " + bmi.toFixed(2)); // .toFixed(2) limits the number of decimals to 2
+// console.log("Category: " + category);
+
+// var secretNumber = 7;
+// var guess = 5; // The player's guess, change this to see that different code lines are executed based on the conditions
+// if (guess === secretNumber) {
+//   console.log("Congratulations! You guessed the correct number.");
+// } else if (guess < secretNumber) {
+//   console.log("Try a higher number.");
+// } else {
+//   console.log("Try a lower number.");
+// }
+// for (var i = 0; i <= 15; i++) {
+//     console.log("Value of i is: " + i);
+//  }
+//  for (var i = 7; i <= 31; i++) {
+//     if (i % 2 != 0) {
+//       console.log(i);
+//     }
+//   }
+for (var i = 10; i >= -20; i--) {
+    if (i % 2 == 0) {
+      console.log(i);
+    }
+  }
+
